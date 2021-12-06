7393556060239cead9562e7228a09b5912f398d7
Marcx <haitianhero6@gmail.com>
2021-12-06 07:03:09 -0500

    updated app.js and candymachine index.js

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Connection, PublicKey } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import { MintLayout, TOKEN_PROGRAM_ID, Token } from '@solana/spl-token';
@@ -9,6 +9,64 @@ import {
   TOKEN_METADATA_PROGRAM_ID,
   SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
 } from './helpers';
+
+const CandyMachine = ({ walletAddress }) => {
+
+  
+  useEffect(() => {
+    getCandyMachineState();
+  }, []);
+}
+
+const getProvider = () => {
+  const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST;
+  // create a new connection object
+  const connection = new Connection(rpcHost);
+  //create a new solana provider object
+  const provider = new Provider (
+    connection,
+    window.solana,
+    opts.preflightCommitment
+  );
+
+  return provider;
+}
+
+const getCandyMachineState = async () => {
+  const provider = getProvider();
+
+  //get metadata about your deployed candy machine program
+  const idl = await Program.fetchIdl(candyMachineProgram, provider);
+
+  //create a program that you can call
+  const program = new Program(idl, candyMachineProgram, provider);
+
+  //fetch the metadata from your candy machine.
+  const candyMachine = await program.account.candyMachine.fetch(
+    process.env.REACT_APP_CANDY_MACHINE_ID
+  );
+
+  //parse out all our metadata and log it out
+  const itemsAvailable = candyMachine.data.itemsAvailable.toNumber();
+  const itemsRedeemed = candyMachine.itemsRedeemed.toNumber();
+  const itemsRemaining = itemsAvailable - itemsRedeemed;
+  const goLiveData = candyMachine.data.goLiveDate.toNumber();
+
+  //We will be using this later in our UI so let's generate this now 
+  const goLiveDateTimeString = `${new Date(
+    goLiveData * 1000
+  ).toGMTString()}`
+
+  console.log({
+    itemsAvailable,
+    itemsRedeemed,
+    itemsRemaining,
+    goLiveData,
+    goLiveDateTimeString,
+  });
+
+};
+
 const {
   metadata: { Metadata, MetadataProgram },
 } = programs;
